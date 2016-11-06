31554e4cb2d261bcef248aec2c5c01b4a9852135
Timothy Baney <timbaney1989@gmail.com>
2016-11-05 20:02:28 -0500

    Add expiration response header

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,10 +1,13 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def view_home(request):
+    expiry_date = datetime.datetime.now() + datetime.timedelta(days=7)
     response = render(request, "index.html", {})
 
     response['Cache-Control'] = 'max-age=602000'
+    response['Expires'] = expiry_date
 
     return response

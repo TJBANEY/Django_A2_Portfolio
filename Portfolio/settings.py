074b815652b9c671386a883ef2110d5770f91b9d
Timothy Baney <timbaney1989@gmail.com>
2016-11-06 00:59:31 -0500

    more settings edits

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -128,7 +128,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "Portfolio/static")
 ]
 

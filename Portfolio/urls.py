c2b815954d55d29fd782ca98d4aace17fcc45362
Timothy Baney <timbaney1989@gmail.com>
2016-11-06 01:19:47 -0500

    re add static directories config

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 
@@ -37,4 +39,4 @@ urlpatterns = [
     url(r'^api/v1/', include(router.urls), name='api'),
     url(r'^api-auth/', include('rest_framework.urls'), name='rest_framework'),
     url(r'^blog', include('blog.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

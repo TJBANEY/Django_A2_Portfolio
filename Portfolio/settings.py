1c2abb234d042c92fe0087d979efb23c8f3e3850
Timothy Baney <timbaney1989@gmail.com>
2017-04-23 16:41:28 -0500

    Edit allowed hosts.

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'f&i!e#l=_$cihdwx73q&vh8ja_yi$flgth^fx05wysa59yrz+7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['timbaney.webfactional.com']
+ALLOWED_HOSTS = ['timbaney1989.webfactional.com']
 
 # Application definition
 
